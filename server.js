af2a7246e6852eda4076647b4f4f8b98ac1e44ee
emma lapalm <lillypadcrafts@gmail.com>
2019-04-27 18:57:53 -0700

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
